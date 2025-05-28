213ac0de95e6d2fe767de09c6b944a35fee90611
Webhooked <hello@webhooked.dev>
2025-05-28 13:18:39 +0200

    removed unused telescope configs

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -6,8 +6,4 @@ return {
     vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
     vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
   end,
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-  },
 }
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,14 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = { "jose-elias-alvarez/typescript.nvim" },
   opts = function()
-    local Keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- stylua: ignore
-    vim.list_extend(Keys, {
-      { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
-      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
-      { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
-      { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
-    })
     return {
       inlay_hints = { enabled = false },
       codelens = { enabled = true },
