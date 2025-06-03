6e9480a6b320bb26eebd5f20d0e143e306eda4c4
Webhooked <hello@webhooked.dev>
2025-06-03 16:04:22 +0200

    Update diffview keymaps

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -4,5 +4,34 @@ return {
     { "<leader>da", mode = { "n", "v" }, "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
     { "<leader>dfh", mode = { "n", "v" }, "<cmd>DiffviewFileHistory %<CR>", desc = "File history" },
     { "<leader>dbh", mode = { "n", "v" }, "<cmd>DiffviewFileHistory<CR>", desc = "Current branch history" },
+    { "<leader>dq", mode = { "n", "v" }, "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+  },
+  opts = {
+    keymaps = {
+      view = {
+        -- Custom navigation keymaps
+        ["<leader>dm"] = "select_next_entry", -- Next file (was ]f)
+        ["<leader>dn"] = "select_prev_entry", -- Previous file (was [f)
+        ["<leader>dt"] = function()
+          require("diffview.config").actions.goto_file_tab()
+        end, -- Open file in new tab
+        -- Disable default keymaps
+        ["]f"] = false,
+        ["[f"] = false,
+        ["<tab>"] = false,
+        ["<s-tab>"] = false,
+      },
+      file_panel = {
+        -- Custom navigation keymaps for file panel
+        ["<leader>dm"] = "select_next_entry", -- Next file (was <tab>)
+        ["<leader>dn"] = "select_prev_entry", -- Previous file (was <s-tab>)
+        ["<leader>dt"] = function()
+          require("diffview.config").actions.goto_file_tab()
+        end, -- Open file in new tab
+        -- Disable default keymaps
+        ["<tab>"] = false,
+        ["<s-tab>"] = false,
+      },
+    },
   },
 }
